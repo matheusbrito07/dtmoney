9db12d6ec1b3f432ff3e3e420b6bb970fb7d92c5
matheusbrito07 <matheus.rja@hotmail.com>
2022-03-25 08:09:31 -0300

    add axios and mirage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { Dashboard } from './components/DashBoard';
 import { Header } from './components/Header';
-
+import { createServer } from 'miragejs'
 import { GlobalStyle } from './styles/global';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    
+    this.get('/transactions', () => {
+      return[
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount:400,
+          type:'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 export function App() {
   return (
